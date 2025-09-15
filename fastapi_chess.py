1af2be626bc6ec528c8fe14b404cc6d4c42f8673
jimbuu <cem.kuel1407@gmail.com>
2025-09-15 13:25:38 +0200

    Funktion zum schließen von db hinzugefügt

--- fastapi_chess.py
+++ fastapi_chess.py
@@ -10,7 +10,13 @@ def get_db_connection():
         database=DB_NAME,
         user=DB_USER,
         password=DB_PASSWORD)
-    return conn
+    cur = conn.cursor()
+    return conn, cur
+
+def close_db_connection(conn, cur):
+    cur.close()
+    conn.close()
+
 
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_NAME = os.getenv("DB_NAME", "postgres")
@@ -32,8 +38,7 @@ def create_table():
             );
         """)
     conn_init.commit()
-    cur_init.close()
-    conn_init.close()
+    close_db_connection(conn_init, cur_init)
 
 create_table()
 
@@ -48,28 +53,23 @@ class NewPlayer(BaseModel):
 
 @app.get('/players')
 async def players():
-    conn_init = get_db_connection()
-    cur_init = conn_init.cursor()
+    conn_init, cur_init = get_db_connection()
     cur_init.execute("SELECT * FROM chess_players;")
     players_data = cur_init.fetchall()
-    cur_init.close()
-    conn_init.close()
+    close_db_connection(conn_init, cur_init)
     return players_data
 
 @app.get('/maxplayer/{rating_type}')
 async def get_max_player(rating_type:str):
-    conn_init = get_db_connection()
-    cur_init = conn_init.cursor()
+    conn_init, cur_init = get_db_connection()
     cur_init.execute(f"SELECT * FROM chess_players ORDER BY {rating_type} DESC;")
     players_data = cur_init.fetchone()
-    cur_init.close()
-    conn_init.close()
+    close_db_connection(conn_init, cur_init)
     return players_data
 
 @app.get('/player')
 async def player(name:str = None, vorname:str = None):
-    conn_init = get_db_connection()
-    cur_init = conn_init.cursor()
+    conn_init, cur_init = get_db_connection()
     player_data = dict()
     print(vorname, name)
     if name != None:
@@ -88,18 +88,15 @@ async def player(name:str = None, vorname:str = None):
             print("Eingegebener Spieler existiert nicht")
         elif result != None:
             player_data = {"ID": result[0], "Vorname": result[1], "Nachname": result[2], "Classical": result[3], "Rapid": result[4], "Blitz": result[5]}
-    cur_init.close()
-    conn_init.close()
+    close_db_connection(conn_init, cur_init)
     return player_data
 
 @app.post('/newplayers')
 async def newplayer(player: NewPlayer):
-    conn_init = get_db_connection()
-    cur_init = conn_init.cursor()
+    conn_init, cur_init = get_db_connection()
     new_player = ("INSERT INTO chess_players (vorname, name, classical, rapid, blitz) VALUES (%s, %s, %s, %s, %s);")
     player_data = (player.vorname, player.name, player.classical, player.rapid, player.blitz )
     cur_init.execute(new_player, player_data)
     conn_init.commit()
-    cur_init.close()
-    conn_init.close()
+    close_db_connection(conn_init, cur_init)
 
