c748aa0ff76744427ff52daec14f8bd33fdf8ee4
jimbuu <cem.kuel1407@gmail.com>
2025-09-22 12:35:06 +0200

    Auf 2 Endpunkte zugegriffen und Daten für Spieler Hikaru Nakamura ausgewertet und in DB eingetragen

---
+++ requestapi.py
@@ -0,0 +1,55 @@
+import requests
+import os
+import psycopg2
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+}
+
+def get_db_connection():
+    conn = psycopg2.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD)
+    cur = conn.cursor()
+    return conn, cur
+
+def close_db_connection(conn, cur):
+    cur.close()
+    conn.close()
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_NAME = os.getenv("DB_NAME", "postgres")
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "EM:IwltdoM,jnoi")
+DB_PORT = os.getenv("DB_PORT", 5432)
+
+def instert_into_table(name, dipar, ztilb, tellub):
+    conn_init, cur_init = get_db_connection()
+    cur_init.execute(f"INSERT INTO chess_players(vorname, name, rapid, blitz, bullet) VALUES ('{name[0]}','{name[1]}',{dipar},{ztilb},{tellub})")
+    conn_init.commit()
+    close_db_connection(conn_init, cur_init)
+
+def get_player_name():
+    response = requests.get(url="https://api.chess.com/pub/player/hikaru", headers=headers)
+    player_data = response.json()
+    name = player_data['name']
+    name = name.split()
+    print(f'Name: {name[0]}')
+    return name
+
+def get_player_elo():
+    response2 = requests.get(url="https://api.chess.com/pub/player/hikaru/stats", headers=headers)
+    player_stats = response2.json()
+    rapid = player_stats['chess_rapid']['last']['rating']
+    blitz = player_stats['chess_blitz']['last']['rating']
+    bullet = player_stats['chess_bullet']['last']['rating']
+    print(f'Rapid rating: {rapid}')
+    return rapid, blitz, bullet
+
+if __name__ == "__main__":
+    name = get_player_name()
+    rapid, blitz, bullet = get_player_elo()
+    instert_into_table(name, rapid, blitz, bullet)
