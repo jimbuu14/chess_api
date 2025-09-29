24f44e51ddc2228ded8d26333ccb67585ce44422
jimbuu <cem.kuel1407@gmail.com>
2025-09-29 12:38:43 +0200

    Code in Teilfunktionen unterteilt

--- requestapi.py
+++ requestapi.py
@@ -12,49 +12,45 @@ def get_best_player_and_post_in_db():
 
     post_best_players_in_db(url_single_player, player_data)
 
+
+def get_player_name(rapid_player):
+    name = rapid_player.get("name")
+    if name == None:
+        name = ("None", "None")
+    else:
+        name = name.split()
+
+    return name[1], name[0]
+
+
+def get_blitz_or_bullet_elo(url_single_player, player_data, username, modus):
+    for player in player_data[f"live_{modus}"]:
+        if player["username"] == username:
+            return player["score"]
+
+    response_player = requests.get(url=f"{url_single_player}{username}/stats",headers=headers)
+    response_data = response_player.json()
+
+    if f"chess_{modus}" in response_data:
+        return response_data[f"chess_{modus}"]["last"]["rating"]
+    else:
+        return 0
    
+
 def post_best_players_in_db(url_single_player, player_data):   
     for rapid_player in player_data["live_rapid"]:
-        player_dict = {"username": "","vorname": "", "name": "", "rapid": 0, "blitz": 0, "bullet": 0}
-        player_dict["username"] = rapid_player["username"]
+        player_dict = {
+            "username": rapid_player["username"],
+            "vorname": "", 
+            "name": "", 
+            "rapid": rapid_player["score"], 
+            "blitz": 0, 
+            "bullet": 0
+            }
 
-        name = rapid_player.get("name")
-        if name == None:
-            name = ("None", "None")
-        else:
-            name = name.split()
-
-        player_dict["name"] = name[1]
-        player_dict["vorname"] = name[0]
-        player_dict["rapid"] = rapid_player["score"]
-
-        for blitz_player in player_data["live_blitz"]:
-            if blitz_player["username"] == player_dict["username"]:
-                player_dict["blitz"] = blitz_player["score"]
-
-        if player_dict["blitz"] == 0:
-            username = player_dict["username"]
-            response_blitz = requests.get(url=f"{url_single_player}{username}/stats",headers=headers)
-            response_data = response_blitz.json()
-
-            if "chess_blitz" in response_data:
-                player_dict["blitz"] = response_data["chess_blitz"]["last"]["rating"]
-            else:
-                player_dict["blitz"] = 0
-        
-        for bullet_player in player_data["live_bullet"]:
-            if bullet_player["username"] == player_dict["username"]:
-                player_dict["bullet"] = bullet_player["score"]
-
-        if player_dict["bullet"] == 0:
-            username = player_dict["username"]
-            response_bullet = requests.get(url=f"{url_single_player}{username}/stats",headers=headers)
-            response_data = response_bullet.json()
-
-            if "chess_bullet" in response_data:
-                player_dict["bullet"] = response_data["chess_bullet"]["last"]["rating"]
-            else:
-                player_dict["bullet"] = 0
+        player_dict["name"], player_dict["vorname"] = get_player_name(rapid_player)
+        player_dict["blitz"] = get_blitz_or_bullet_elo(url_single_player, player_data, player_dict["username"], "blitz")
+        player_dict["bullet"] = get_blitz_or_bullet_elo(url_single_player, player_data, player_dict["username"], "bullet")
 
         requests.post(url="http://localhost:8000/newplayers", json=player_dict)
 
